d7e305f363842d6b721af0fad9106eba8d1d042d
Sowjanya Sode <sowjanya.sode@gmail.com>
2018-02-19 21:01:34 -0500

    checkin changes

---
+++ HelloWorldProvider/HelloWorldProvider/HelloWorld.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldProvider
+{
+    /// <summary>
+    /// testxcvxcv
+    /// </summary>
+    public class HelloWorld
+    {
+        public string SayHello()
+        {
+            return SayHello("HelloWorld");
+        }
+
+
+        public string SayHello(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
